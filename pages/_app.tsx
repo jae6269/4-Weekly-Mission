6b52ca97e62a45a938782073cd3f09cff9c8cc04
jae6269 <jae6269@gmail.com>
2024-05-19 00:28:30 +0900

    feat: _app.js tanstack query setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css';
+import React from 'react';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // 보통 SSR에서는 staleTime을 0 이상으로 해줌으로써
+            // 클라이언트 사이드에서 바로 다시 데이터를 refetch 하는 것을 피한다.
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
